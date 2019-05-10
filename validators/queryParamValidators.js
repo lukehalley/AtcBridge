b3b6b1501b6f926f82dd5e27f7bcf64f57795e6e
Luke Halley <luke123halley@gmail.com>
2019-05-10 10:01:44 +1000

    Document parameter validation logic

--- validators/queryParamValidators.js
+++ validators/queryParamValidators.js
@@ -9,6 +9,7 @@
  * Validates and sanitizes incoming request parameters
  */
 // Ensure all required query parameters are present and valid before processing
+// Validate query parameters against schema requirements
 // Validate required query parameters and ensure type safety for API endpoint
 /**
  * Validates query parameters against defined schemas
