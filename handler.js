31d07cb9a8150365f8da6466386015de65a836e7
Luke Halley <luke123halley@gmail.com>
2020-05-01 10:50:20 +1000

    Add JSDoc for main handler function

--- handler.js
+++ handler.js
@@ -1,4 +1,10 @@
 /**
+ * Main serverless handler for API requests
+ * @param {Object} event - Lambda event object
+ * @param {Object} context - Lambda context object
+ * @returns {Promise<Object>} API response
+ */
+/**
  * Main handler for API requests
  * @param {Object} event - Lambda event object
  * @param {Object} context - Lambda context object
