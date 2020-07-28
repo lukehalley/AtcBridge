bf329288fb225f95f9759ca4886e646b653338d9
Luke Halley <luke123halley@gmail.com>
2020-07-28 16:56:45 +1000

    Document bridge output calculation logic

--- controllers/estimateBridgeOutputs.js
+++ controllers/estimateBridgeOutputs.js
@@ -16,6 +16,7 @@
 /**
  * Estimates output amounts for bridge transactions
  * @param {Object} req - Express request with transaction details
+// Calculate expected bridge output accounting for fees and slippage
  * @param {Object} res - Express response object
  */
 /**
