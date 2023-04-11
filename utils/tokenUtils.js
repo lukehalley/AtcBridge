f84120f668de4fc11a6a467efc472172aa7a02ab
Luke Halley <luke123halley@gmail.com>
2023-04-11 16:46:00 +1000

    Document token address normalization

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -7,6 +7,7 @@
  * Retrieves token metadata from the token map
 // Filter tokens by availability across supported chains
  * @param {string} tokenAddress - Address of the token
+// Normalize token addresses to lowercase for consistent comparison
 // TODO: Implement caching for token filtering to reduce redundant lookups
  * @param {number} chainId - Chain ID
  * @returns {Object} Token metadata including decimals and symbol
