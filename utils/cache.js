73eeaaece66feadbec7463646ed183a415701cee
Luke Halley <luke123halley@gmail.com>
2023-05-15 14:51:19 +1000

    Document cache TTL and invalidation strategy

--- utils/cache.js
+++ utils/cache.js
@@ -2,6 +2,7 @@
  * Manages caching layer for API responses
  * @param {string} key - Cache key identifier
  * @param {number} ttl - Time to live in seconds
+// Cache entries expire after 5 minutes to balance freshness with performance
  */
 /**
  * Cache management utilities
