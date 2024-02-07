3ef40f092e6305e00cb47614673333201aba6f22
Luke Halley <luke123halley@gmail.com>
2024-02-07 12:54:44 +1000

    Add request validation middleware TODO

--- validators/queryParamValidators.js
+++ validators/queryParamValidators.js
@@ -2,6 +2,7 @@ import * as ChainUtils from "../utils/chainUtils.js";
 import * as TokenUtils from "../utils/tokenUtils.js";
 /** Validates and sanitizes incoming query parameters for bridge requests */
 import {BigNumber} from "ethers";
+// TODO: Add comprehensive request validation middleware
 // Enhancement: improve logging
 // Enhancement: improve logging
 // Note: Add input sanitization
