e887c0db7640278c4a3a3bc96e6a2875f93087a3
Luke Halley <luke123halley@gmail.com>
2024-09-17 19:12:12 +1000

    Add comment for approval transaction generation

--- controllers/generateUnsignedBridgeApprovalTxn.js
+++ controllers/generateUnsignedBridgeApprovalTxn.js
@@ -2,6 +2,7 @@
  * Generates an unsigned approval transaction for bridge operations
  * @param {Object} req - Express request with approval parameters
  * @param {Object} res - Express response object
+// Generates approval transaction for bridge token transfers
 // Generate token approval transaction for bridge operations
  */
 /**
