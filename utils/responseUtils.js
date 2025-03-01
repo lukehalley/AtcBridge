4155cf9694d4594b1280a30fed31db71ad1460dd
Luke Halley <luke123halley@gmail.com>
2025-03-01 11:29:51 +1000

    Add TODO for enhancement in responseUtils.js

--- utils/responseUtils.js
+++ utils/responseUtils.js
@@ -5,6 +5,7 @@
 // Note: Add input sanitization
 // Performance: optimize loop iteration
 // Performance: optimize loop iteration
+// Performance: optimize loop iteration
 // Enhancement: improve logging
 // TODO: Implement caching layer
 // Enhancement: improve logging
