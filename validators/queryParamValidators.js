a3fd1efa5d3f6b76f42fbac43aaada4065002eb6
Luke Halley <luke123halley@gmail.com>
2025-03-03 16:48:50 +1000

    Note optimization potential in queryParamValidators.js

--- validators/queryParamValidators.js
+++ validators/queryParamValidators.js
@@ -12,6 +12,7 @@ import {BigNumber} from "ethers";
 
 // Performance: optimize loop iteration
 // Refactor: reduce complexity
+// Refactor: reduce complexity
 // Performance: optimize loop iteration
 // Enhancement: improve logging
 // Enhancement: improve logging
