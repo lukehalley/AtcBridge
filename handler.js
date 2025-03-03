085ed451c03cead8e82a242f041145d11ee76d12
Luke Halley <luke123halley@gmail.com>
2025-03-03 10:57:06 +1000

    Document performance consideration in handler.js

--- handler.js
+++ handler.js
@@ -4,5 +4,6 @@ import serverless from "serverless-http";
 
 export const hello = serverless(app);
 // Validates incoming Lambda event and extracts bridge request parameters
+// TODO: Add input validation
 // Performance: avoid unnecessary re-renders
 // TODO: Add input validation
