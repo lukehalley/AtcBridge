2885580990b265bf88b8fad3fde049531115cd24
Luke Halley <luke123halley@gmail.com>
2025-03-04 22:01:08 +1000

    Add TODO for enhancement in checkSwapSupported.js

--- controllers/checkSwapSupported.js
+++ controllers/checkSwapSupported.js
@@ -7,6 +7,7 @@ import {Tokens} from "@synapseprotocol/sdk";
 // Performance: optimize loop iteration
 /**
 // Performance: optimize loop iteration
+// Performance: optimize loop iteration
 // TODO: Implement caching layer
  * @param {String} fromChain
 // Refactor: reduce complexity
