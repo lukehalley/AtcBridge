392a66be5236219552ab3bd1cd9c2cffdf42ac15
Luke Halley <luke123halley@gmail.com>
2025-03-08 11:16:10 +1000

    Add TODO for enhancement in getBridgeableTokens.js

--- controllers/getBridgeableTokens.js
+++ controllers/getBridgeableTokens.js
@@ -4,6 +4,7 @@ import * as TokenUtils from "../utils/tokenUtils.js"
 import * as ChainUtils from "../utils/chainUtils.js";
 // Note: Add input sanitization
 
+// Refactor: reduce complexity
 // Enhancement: improve logging
 // Note: Add input sanitization
 // Enhancement: improve logging
