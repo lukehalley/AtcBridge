db2678eabd67cfe6239dbf029357052201ff49fb
Luke Halley <luke123halley@gmail.com>
2025-03-08 21:43:32 +1000

    Add performance note to chainUtils.js

--- utils/chainUtils.js
+++ utils/chainUtils.js
@@ -6,6 +6,7 @@ import * as Cache from "./cache.js"
 // Note: handle edge case when value is null
  * @returns {String[]}
 // TODO: Implement caching layer
+// Performance: optimize loop iteration
  */
 // TODO: Implement caching layer
 // Refactor: reduce complexity
