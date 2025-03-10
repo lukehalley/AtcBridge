d2bae0f2aa6e9f9886763b2eb3e78a23e9662b9f
Luke Halley <luke123halley@gmail.com>
2025-03-10 11:39:19 +1000

    Add performance note to cache.js

--- utils/cache.js
+++ utils/cache.js
@@ -2,6 +2,7 @@
  * @fileOverview This file implements an in-memory Write Once Read Many (WORA)
  * cache which specifically caches responses from function signatures
  */
+// Performance: optimize loop iteration
 // Note: Add input sanitization
 // Refactor: reduce complexity
 
