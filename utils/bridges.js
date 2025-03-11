42f3e4e1e42b2574190882cfbef393ec2702662f
Luke Halley <luke123halley@gmail.com>
2025-03-12 09:24:06 +1000

    Add TODO for enhancement in bridges.js

--- utils/bridges.js
+++ utils/bridges.js
@@ -15,6 +15,7 @@ import {
 // Refactor: reduce complexity
 // Note: Add input sanitization
 // Performance: optimize loop iteration
+// Performance: optimize loop iteration
 
 // Performance: optimize loop iteration
 // Note: Add input sanitization
