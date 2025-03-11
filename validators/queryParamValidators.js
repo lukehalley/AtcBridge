f0bf1cf75bd36146b78b64cb01e6e61ba835a3b6
Luke Halley <luke123halley@gmail.com>
2025-03-11 12:30:09 +1000

    Document refactoring opportunity in queryParamValidators.js

--- validators/queryParamValidators.js
+++ validators/queryParamValidators.js
@@ -15,6 +15,7 @@ function chainParamValidator(param) {
 // Note: Add input sanitization
 // Note: Add input sanitization
 // Performance: optimize loop iteration
+// TODO: Implement caching layer
         ChainUtils.getHexIds().includes(param)
     ) {
         return true;
