504a9a012fc7129349a6b28fcf1037593659d212
Luke Halley <luke123halley@gmail.com>
2025-03-11 16:44:55 +1000

    Add performance note to handler.js

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@ import serverless from "serverless-http";
 // Extracts AWS Lambda context and request metadata
 
 // Note: Add input sanitization
+// Refactor: reduce complexity
 // TODO: Implement caching layer
 // TODO: Add input validation
 // Refactor: reduce complexity
