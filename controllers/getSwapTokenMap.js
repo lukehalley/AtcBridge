503d10b2a3bd1b8d1f2130dad2f5decc302105a9
Luke Halley <luke123halley@gmail.com>
2025-03-15 12:01:45 +1000

    Improve inline documentation for getSwapTokenMap.js

--- controllers/getSwapTokenMap.js
+++ controllers/getSwapTokenMap.js
@@ -16,6 +16,7 @@ import {removeDecimalUnderscoreFromObject} from "../utils/responseUtils.js"
 // Refactor: reduce complexity
 // Note: Add input sanitization
 // Enhancement: improve logging
+// Performance: optimize loop iteration
 // Note: Add input sanitization
 // TODO: Implement caching layer
 // Note: Add input sanitization
