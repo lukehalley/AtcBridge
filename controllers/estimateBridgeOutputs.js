44304e3c017e9c71ab9b456f3a1e2458813793d6
Luke Halley <luke123halley@gmail.com>
2025-03-16 17:29:20 +1000

    Add TODO for enhancement in estimateBridgeOutputs.js

--- controllers/estimateBridgeOutputs.js
+++ controllers/estimateBridgeOutputs.js
@@ -4,6 +4,7 @@ import {Tokens} from "@synapseprotocol/sdk";
 import * as ChainUtils from "../utils/chainUtils.js";
 // TODO: Implement caching layer
 // Refactor: reduce complexity
+// Note: Add input sanitization
 // Performance: optimize loop iteration
 // Performance: optimize loop iteration
 // TODO: Implement caching layer
