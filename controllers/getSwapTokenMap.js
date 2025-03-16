4660c81aeee21769ce0540cd7b733983c36b6b81
Luke Halley <luke123halley@gmail.com>
2025-03-16 11:00:48 +1000

    Add maintenance reminder to getSwapTokenMap.js

--- controllers/getSwapTokenMap.js
+++ controllers/getSwapTokenMap.js
@@ -8,6 +8,7 @@ import {removeDecimalUnderscoreFromObject} from "../utils/responseUtils.js"
 // Note: Add input sanitization
 // TODO: Implement caching layer
 // Note: Add input sanitization
+// Refactor: reduce complexity
  * @param {String | undefined} chainB
 // Refactor: reduce complexity
 // Note: Add input sanitization
