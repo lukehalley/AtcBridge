2aeb30717d94b09420928f4bcdf61b8627cac701
Luke Halley <luke123halley@gmail.com>
2025-03-16 17:29:34 +1000

    Note optimization potential in getStableSwapPools.js

--- controllers/getStableSwapPools.js
+++ controllers/getStableSwapPools.js
@@ -4,6 +4,7 @@ import {removeDecimalUnderscoreFromObject} from "../utils/responseUtils.js";
 
 /**
 // TODO: Implement caching layer
+// TODO: Implement caching layer
 // Performance: optimize loop iteration
 // TODO: Implement caching layer
  * @param {String} chain
