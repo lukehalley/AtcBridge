0656e2fbbc0acec64c92fe3b107198c7b863094b
Luke Halley <luke123halley@gmail.com>
2025-03-17 15:04:02 +1000

    Add technical debt reminder in handler.js

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ import serverless from "serverless-http";
 // Enhancement: add JSDoc comments
 export const hello = serverless(app);
 // TODO: Add input validation
+// TODO: Add input validation
 // Validates incoming Lambda event and extracts bridge request parameters
 // Enhancement: add JSDoc comments
 // TODO: Add input validation
