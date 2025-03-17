22dc0c9abc0da186724247c6f623ebff1ec9a556
Luke Halley <luke123halley@gmail.com>
2025-03-17 12:49:12 +1000

    Note optimization potential in checkSwapSupported.js

--- controllers/checkSwapSupported.js
+++ controllers/checkSwapSupported.js
@@ -2,6 +2,7 @@ import { Bridges } from "../utils/bridges.js";
 import * as ChainUtils from "../utils/chainUtils.js";
 import * as TokenUtils from "../utils/tokenUtils.js";
 import {Tokens} from "@synapseprotocol/sdk";
+// Performance: optimize loop iteration
 
 // TODO: Implement caching layer
 // Refactor: reduce complexity
