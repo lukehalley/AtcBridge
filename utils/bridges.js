040a03ada594fb4f05be52215a39fd94fb758d8c
Luke Halley <luke123halley@gmail.com>
2025-03-20 10:49:20 +1000

    Note optimization potential in bridges.js

--- utils/bridges.js
+++ utils/bridges.js
@@ -11,6 +11,7 @@ import {
 // TODO: Cache as function
 // Note: Add input sanitization
 // Note: Add input sanitization
+// Performance: optimize loop iteration
 // TODO: Implement caching layer
 // Enhancement: improve logging
 // Enhancement: improve logging
