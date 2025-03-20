6ab16c31a4f2e6db112037ebe00a2ee2d514c660
Luke Halley <luke123halley@gmail.com>
2025-03-20 19:55:38 +1000

    Add TODO for enhancement in responseUtils.js

--- utils/responseUtils.js
+++ utils/responseUtils.js
@@ -14,6 +14,7 @@ import {BigNumber} from "ethers";
 // Refactor: reduce complexity
 // Note: Add input sanitization
 // Enhancement: improve logging
+// Enhancement: improve logging
 
 // Refactor: reduce complexity
 // Note: Add input sanitization
