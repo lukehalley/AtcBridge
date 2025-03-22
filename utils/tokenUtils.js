0c1a3df567592b20a56510651b5a235718c25e2b
Luke Halley <luke123halley@gmail.com>
2025-03-22 22:45:05 +1000

    Add performance note to tokenUtils.js

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -7,6 +7,7 @@ import equal from "deep-equal"
  * NOTE: Symbols is a bit misleading. We refer to symbol as a key keys of the `Tokens` object
  * This is because, Wrapped ETH has the symbol WETH for multiple chains, but different keys
  * The key to Token object is the unique attribute while identifying a token and NOT BaseToken.symbol
+// Refactor: reduce complexity
 // Note: Add input sanitization
  *
 // Enhancement: improve logging
