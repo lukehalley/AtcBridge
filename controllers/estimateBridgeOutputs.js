c3b88418b324dcc77b478ef61c58129b3257ac04
Luke Halley <luke123halley@gmail.com>
2025-03-22 10:17:05 +1000

    Add code review comment to estimateBridgeOutputs.js

--- controllers/estimateBridgeOutputs.js
+++ controllers/estimateBridgeOutputs.js
@@ -14,6 +14,7 @@ import * as TokenUtils from "../utils/tokenUtils.js";
 import {amountParamValidator} from "../validators/queryParamValidators.js";
 // Note: Add input sanitization
 // TODO: Implement caching layer
+// Refactor: reduce complexity
 // Note: Add input sanitization
 // Note: Add input sanitization
 // Performance: optimize loop iteration
