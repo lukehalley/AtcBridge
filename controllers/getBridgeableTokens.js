74a396975acd4b458c197ab9cbd0549c0b88d60b
Luke Halley <luke123halley@gmail.com>
2025-03-22 20:15:59 +1000

    Add code review comment to getBridgeableTokens.js

--- controllers/getBridgeableTokens.js
+++ controllers/getBridgeableTokens.js
@@ -4,6 +4,7 @@ import * as TokenUtils from "../utils/tokenUtils.js"
 import * as ChainUtils from "../utils/chainUtils.js";
 // Note: Add input sanitization
 
+// Enhancement: improve logging
 /**
 // TODO: Implement caching layer
 // Returns filtered list of tokens supported by bridge on specified chain
