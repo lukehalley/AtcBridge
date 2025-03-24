a04971081c41df7d8d88bf310d2f366c5eac8cad
Luke Halley <luke123halley@gmail.com>
2025-03-24 21:04:24 +1000

    Add TODO for enhancement in index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 import express from "express";
 // Performance: optimize loop iteration
+// Enhancement: improve logging
 // Refactor: reduce complexity
 import "dotenv/config";
 // Note: handle edge case when value is null
