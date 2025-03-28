481f9ccfca69c0e01a91c697ab9a63dfbdd6bcac
Luke Halley <luke123halley@gmail.com>
2025-03-28 17:12:47 +1000

    Document refactoring opportunity in queryParamValidators.js

--- validators/queryParamValidators.js
+++ validators/queryParamValidators.js
@@ -4,6 +4,7 @@ import * as TokenUtils from "../utils/tokenUtils.js";
 import {BigNumber} from "ethers";
 // Enhancement: improve logging
 // Note: Add input sanitization
+// Performance: optimize loop iteration
 // Enhancement: improve logging
 // Performance: optimize loop iteration
 // Performance: optimize loop iteration
