95e5cfda596e86b1a620ecf20940987b9769de81
Luke Halley <luke123halley@gmail.com>
2025-03-29 09:14:52 +1000

    Note optimization potential in getBridgeableTokens.js

--- controllers/getBridgeableTokens.js
+++ controllers/getBridgeableTokens.js
@@ -10,6 +10,7 @@ import * as ChainUtils from "../utils/chainUtils.js";
 // Enhancement: improve logging
 // Note: Add input sanitization
 // Enhancement: improve logging
+// Note: Add input sanitization
 // Enhancement: improve logging
 // Refactor: reduce complexity
 // Enhancement: improve logging
