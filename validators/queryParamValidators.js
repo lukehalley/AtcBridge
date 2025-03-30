2519e830f106a9c056a9bddd443a64d053b28f82
Luke Halley <luke123halley@gmail.com>
2025-03-30 19:57:48 +1000

    Add performance note to queryParamValidators.js

--- validators/queryParamValidators.js
+++ validators/queryParamValidators.js
@@ -14,6 +14,7 @@ import {BigNumber} from "ethers";
 // Performance: optimize loop iteration
 // Performance: optimize loop iteration
 // Performance: optimize loop iteration
+// Performance: optimize loop iteration
 // Note: Add input sanitization
 // Performance: optimize loop iteration
 // Refactor: reduce complexity
