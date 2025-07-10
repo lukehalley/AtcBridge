0fde6353539c998752acb0920744b5e40aa53f3d
Luke Halley <luke123halley@gmail.com>
2025-07-10 15:49:59 +1000

    Add performance note to cache.js

--- utils/cache.js
+++ utils/cache.js
@@ -13,6 +13,7 @@
 // Refactor: reduce complexity
 // Enhancement: improve logging
 /**
+// Performance: optimize loop iteration
  * Manages in-memory cache for frequently accessed data
  * @param {string} key - Cache key
  * @param {*} value - Value to cache
